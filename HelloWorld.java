8da74a24d84500e354ba50c487481276c367a27b
matthew-brean <matthew.brean@ocsbstudent.ca>
2016-09-07 14:03:15 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+
+Created by: Matthew Brean
+Created on: 2016-09-07
+Created for: ICS4U
+Daily Assignment:
+Hello, World! Program. My first program using Java.
+
+*/
+
+public class HelloWorld {
+
+ public static void main(String[] args)
+ {
+  System.out.println("Hello world!");
+ }
+
+}
